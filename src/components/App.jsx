3b716b032021bd23f263822e628aa267ac087f03
Mukola <koliunia2006@gmail.com>
2023-10-02 21:08:51 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import './style.css'
+
+class ColorChanger extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: 'lightgray',
+    };
+  }
+  generateRandomColor = () => {
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+  }
+
+
+  changeColor = () => {
+    const newColor = this.generateRandomColor();
+    this.setState({ backgroundColor: newColor });
+  }
+
+  render() {
+    const { backgroundColor } = this.state;
+    return (
+      <div className="centered-container">
+        <button
+          onClick={this.changeColor}
+          style={{ backgroundColor: backgroundColor }}
+          className="color-button"
+        >
+          Змінити колір
+        </button>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ColorChanger />
+      </div>
+    );
+  }
+}
+
+export default App;
